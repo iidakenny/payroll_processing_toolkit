a2fe2a6d70cacff2e75e4dd9c43ccdb5b46709f1
Iida Kenny <iida@usf.edu>
2026-02-20 14:51:47 -0500

    Crated an array of 3 employee objects with name, hourly rate, and hours worked.

---
+++ cc_5.js
@@ -0,0 +1,14 @@
+// An array of employees with name, hourly rate, and hours worked.
+let employees = [
+  { name: "Alma", 
+    hourlyRate: 23, 
+    hoursWorked: 40 
+},
+  { name: "Bobby", 
+    hourlyRate: 43, 
+    hoursWorked: 35 
+},
+  { name: "Candice",
+    hourlyRate: 37, 
+    hoursWorked: 39 }];
+
