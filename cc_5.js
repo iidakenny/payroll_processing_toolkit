7d4508dfb26d7ff235f4dd0a9b1f0bdf00347331
Iida Kenny <iida@usf.edu>
2026-02-22 15:42:00 -0500

    Created base, overtime, and net pay functions, and created examples on log for employee[2]

--- cc_5.js
+++ cc_5.js
@@ -11,7 +11,12 @@ let employees = [
 },
   { name: "Candice",
     hourlyRate: 37, 
-    hoursWorked: 65 }];
+    hoursWorked: 65
+},
+  { name: "Dracula",
+    hourlyRate: 55,
+    hoursWorked: 51}
+];
 
 
 // Function that claculates the base pay for up to 40 hours. 
@@ -24,9 +29,31 @@ function calculateBasePay(rate, hours) {
     }
  }    
  
- // Testing the function with the first employee, Alma.
- let basePay = calculateBasePay(employees[0].hourlyRate, employees[0].hoursWorked);
- console.log(`Base pay for ${employees[0].name} is $${basePay}`);
+ // Testing the base pay function with an employee.
+ let basePay = calculateBasePay(employees[2].hourlyRate, employees[2].hoursWorked);
+ console.log(`Base pay for ${employees[2].name} is $${basePay}`);
 
 
- 
+ function calculateOvertimePay(rate, hours) {
+    if (hours > 40) {
+        return (rate * 1.5 * (hours - 40)).toFixed(2);
+    } else {
+        return 0;
+    }
+ }
+// Testing the overtime function and returning an employee's base, overtime, and gross pay.
+let overtimePay = calculateOvertimePay(employees[2].hourlyRate, employees[2].hoursWorked);
+let grossPay = (parseFloat(basePay) + parseFloat(overtimePay)).toFixed(2);
+
+console.log(`${employees[2].name}'s paycheck: \nBase pay: $${basePay} | Overtime pay $${overtimePay} | Gross pay: $${grossPay}`);
+
+// Function that deducts 15% tax from gross pay.
+let taxRate = 0.15;
+function calculateNetPay(grossPay) {
+    return (grossPay * (1 - taxRate)).toFixed(2);
+}
+let netPay = calculateNetPay(grossPay);
+
+// Checking that the net pay function works.
+console.log(`${employees[2].name}'s net pay is $${netPay} with a tax deduction of 15%`);
+
